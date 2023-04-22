06d7fbf70fe7e81768b13852acc97ceb90244755
Isaac <icorbrey@gmail.com>
2023-04-21 21:07:39 -0400

    feat: Put header in screen column

--- src/lib/layout/Header.svelte
+++ src/lib/layout/Header.svelte
@@ -1,28 +1,36 @@
 <script lang="ts">
+	import ScreenColumn from './ScreenColumn.svelte';
 </script>
 
 <header>
-	<a href="/"><h1>Isaac Corbrey</h1></a>
-	&nbsp;
-	<a href="/support">Support Me</a>
+	<ScreenColumn>
+		<div class="inner">
+			<a href="/"><h1>Isaac Corbrey</h1></a>
+			&nbsp;
+			<a href="/support">Support Me</a>
+		</div>
+	</ScreenColumn>
 </header>
 
 <style lang="scss">
 	header {
-		display: grid;
-		grid-template-columns: auto 1fr auto;
-		align-items: center;
-
 		box-shadow: 0px 7px 7px 0px rgba(0, 0, 0, 0.3);
-		font-family: 'Nunito Sans', sans-serif;
 
-		a {
-			text-decoration: none;
-			color: black;
-			padding: 7px 14px;
+		.inner {
+			display: grid;
+			grid-template-columns: auto 1fr auto;
+			align-items: center;
 
-			&:hover {
-				text-decoration: underline;
+			font-family: 'Nunito Sans', sans-serif;
+
+			a {
+				text-decoration: none;
+				color: black;
+				padding: 7px 0px;
+
+				&:hover {
+					text-decoration: underline;
+				}
 			}
 		}
 	}
