8494358ee3ad90c16265ec95565da76ca64960b5
Isaac Corbrey <isaac.corbrey@doitbest.com>
2024-10-30 10:03:39 -0400

    chore(deps): Add Netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import netlify from '@astrojs/netlify';
 import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+  integrations: [svelte()],
+  adapter: netlify(),
+  output: 'server',
 });
