c3538e4b25827fdfb3b851de673d5e8a858f775b
Isaac Corbrey <isaac.corbrey@doitbest.com>
2024-10-31 11:02:22 -0400

    feat(style): Set up root layout and base for posts (#1)
    * feat(wip): Changes in progress
    
    * feat(wip): Better styling
    
    * feat(wip): Set up posts

---
+++ src/components/ScreenColumn.svelte
@@ -0,0 +1,24 @@
+<div class="outer">
+	<div class="inner">
+		<slot />
+	</div>
+</div>
+
+<style>
+	.outer {
+		justify-content: center;
+		flex-flow: row nowrap;
+		align-items: stretch;
+		display: flex;
+
+		height: 100%;
+		width: 100%;
+	}
+
+	.inner {
+		flex: 0 1 auto;
+
+		max-width: 55rem;
+		width: 100%;
+	}
+</style>
