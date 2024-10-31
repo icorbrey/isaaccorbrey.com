cc3b4fbe9ec4310470a6daab6f876b3cd1987c21
Isaac Corbrey <isaac.corbrey@doitbest.com>
2024-10-31 11:00:29 -0400

    feat(wip): Set up posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 import svelte from '@astrojs/svelte';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
+  integrations: [svelte(), mdx()],
   adapter: netlify(),
   output: 'server',
 });
