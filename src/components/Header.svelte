5115ae8a45da9258d6f472b5069612fd33d4ee1d
Isaac Corbrey <isaac.corbrey@doitbest.com>
2024-10-31 10:30:17 -0400

    feat(wip): Better styling

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import ScreenColumn from './ScreenColumn.svelte';
+	import ScreenColumn from "./ScreenColumn.svelte";
 </script>
 
 <header>
@@ -7,15 +7,43 @@
 	<ul>
 		<li><a href="/posts">Posts</a></li>
 		<li><a href="/projects">Projects</a></li>
-		<li><a href="/who-am-i">Who am I?</a></li>
+		<li><a href="/about">About me</a></li>
 	</ul>
 </header>
 
 <style>
 	header {
+		justify-content: space-between;
+		flex-flow: row nowrap;
+		align-items: center;
+		display: flex;
+
+		padding: 2rem 0;
+		width: 100%;
+	}
+
+	h1 {
+		font-size: 1.5rem;
+	}
+
+	ul {
 		flex-flow: row nowrap;
 		display: flex;
-		width: 100%;
-		justify-content: space-between;
+
+		gap: 1.5rem;
+	}
+
+	li {
+		list-style: none;
+		font-size: 1.2rem;
+	}
+
+	a {
+		text-decoration: none;
+		color: var(--foreground-2);
+	}
+
+	a:hover {
+		color: var(--foreground-1);
 	}
 </style>
--- src/components/ScreenColumn.svelte
+++ src/components/ScreenColumn.svelte
@@ -1,9 +1,24 @@
-<div>
-	<slot />
+<div class="outer">
+	<div class="inner">
+		<slot />
+	</div>
 </div>
 
 <style>
-	div {
-		max-width: 800px;
+	.outer {
+		justify-content: center;
+		flex-flow: row nowrap;
+		align-items: stretch;
+		display: flex;
+
+		height: 100%;
+		width: 100%;
+	}
+
+	.inner {
+		flex: 0 1 auto;
+
+		max-width: 55rem;
+		width: 100%;
 	}
 </style>
