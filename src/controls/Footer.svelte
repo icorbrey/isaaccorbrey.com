138b98600d143f4c65e7979e7c51176e57072d3f
Isaac Corbrey <isaac.corbrey@doitbest.com>
2024-11-01 13:59:01 -0400

    feat(layout): Create footer

---
+++ src/controls/Footer.svelte
@@ -0,0 +1,69 @@
+<script>
+    import FooterIcon from "./FooterIcon.svelte";
+</script>
+
+<footer>
+    <div class="text-links">
+        <a href="https://ko-fi.com">Buy me a coffee</a>
+        &CenterDot;
+        <a href="https://github.com/sponsors/icorbrey">Sponsor me</a>
+    </div>
+    <div class="icon-links">
+        <FooterIcon
+            href="https://github.com/icorbrey"
+            src="/logos/github.svg"
+            text="GitHub"
+        />
+        <FooterIcon
+            href="https://printables.com/@icorbrey"
+            src="/logos/printables.svg"
+            text="Printables"
+        />
+        <FooterIcon
+            href="https://youtube.com/@icorbrey"
+            src="/logos/youtube.svg"
+            text="YouTube"
+        />
+        <FooterIcon
+            href="https://bsky.app/@icorbrey"
+            src="/logos/bluesky.svg"
+            text="BlueSky"
+        />
+    </div>
+</footer>
+
+<style>
+    footer {
+        flex-flow: column nowrap;
+        align-items: center;
+        display: flex;
+        gap: 0.5rem;
+
+        margin-top: auto;
+        padding: 1.5rem;
+        width: 100%;
+    }
+
+    .text-links {
+        flex-flow: row nowrap;
+        display: flex;
+        gap: 0.5rem;
+
+        color: var(--foreground-2);
+    }
+
+    .text-links a {
+        color: var(--foreground-2);
+    }
+
+    .text-links a:hover {
+        color: var(--foreground-1);
+    }
+
+    .icon-links {
+        flex-flow: row wrap;
+        align-items: center;
+        display: flex;
+        gap: 0.45rem;
+    }
+</style>
