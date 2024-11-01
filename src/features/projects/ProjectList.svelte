1617226760396b76b08fc605e8e495e6486e0404
Isaac Corbrey <isaac.corbrey@doitbest.com>
2024-11-01 10:48:22 -0400

    feat(posts): Add support for drafts (#13)
    * feat(posts): Add support for drafts
    
    * fix: Remove old broken stuff

--- src/features/projects/ProjectList.svelte
+++ src/features/projects/ProjectList.svelte
@@ -1,15 +1,1 @@
-<script lang="ts">
-    type Project = {
-        title: string;
-        href: string;
-        date: string;
-        post: string;
-        demo: string;
-    };
-
-    export let projects: Project[] = [];
-</script>
-
-{#if 0 < projects.length}{:else}
-    <p>Move along, nothing to see here.</p>
-{/if}
+<p>Move along, nothing to see here.</p>
