91e8c1c6f3bb65b70723b0be15111e5a77267718
Isaac Corbrey <isaac.corbrey@doitbest.com>
2024-11-04 12:30:11 -0500

    feat(comments): Add utteranc.es integration (#38)

---
+++ src/controls/Comments.svelte
@@ -0,0 +1,12 @@
+{#if true}
+    <script
+        src="https://utteranc.es/client.js"
+        repo="icorbrey/isaaccorbrey.com"
+        crossorigin="anonymous"
+        issue-term="og:title"
+        theme="github-dark"
+        label="comments"
+        async
+    >
+    </script>
+{/if}
