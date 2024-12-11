c23bb3d468b3c7ffbcc386560aa0851df93c7f02
Isaac Corbrey <icorbrey@gmail.com>
2024-12-11 18:56:11 -0500

    refactor(style): Migrate to Tailwind CSS
    This should make it easier to add on more things in the future and keep
    things consistent.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,25 +3,26 @@ import { postReadingTime } from './src/remark-plugins/post-reading-time.mjs';
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 import svelte from '@astrojs/svelte';
-import mdx from '@astrojs/mdx';
+
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://isaaccorbrey.com',
-	integrations: [svelte(), mdx()],
-	adapter: netlify(),
-	output: 'server',
-	redirects: {
-		'/n/[...slug]': '/notes/[...slug]',
-		'/t/[...slug]': '/tags/[...slug]',
-		'/n': '/notes',
-	},
-	markdown: {
-		remarkPlugins: [
-			postReadingTime,
-		],
-		shikiConfig: {
-			wrap: true,
-		}
-	}
+    site: 'https://isaaccorbrey.com',
+    integrations: [svelte(), tailwind()],
+    adapter: netlify(),
+    output: 'server',
+    redirects: {
+        '/n/[...slug]': '/notes/[...slug]',
+        '/t/[...slug]': '/tags/[...slug]',
+        '/n': '/notes',
+    },
+    markdown: {
+        remarkPlugins: [
+            postReadingTime,
+        ],
+        shikiConfig: {
+            wrap: true,
+        }
+    }
 });
---
+++ tailwind.config.mjs
@@ -0,0 +1,66 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		fontFamily: {
+			display: ['"Afacad Flux"', 'sans-serif'],
+			code: ['"Fira Code"', 'monospace'],
+		},
+		colors: {
+			bg: {
+				1: "#1e1e1e",
+				2: "#282828",
+				3: "#ffffff1c",
+			},
+			fg: {
+				1: "#dcdcdc",
+				2: "#bfbfbf",
+				3: "#8c8c8c",
+				4: "#686868",
+			},
+			ui: {
+				1: "#373737",
+				2: "#515151",
+				3: "#595959",
+			},
+			red: {
+				400: "#ff3b11",
+			},
+			orange: {
+				400: "#ff9502",
+				500: "#e68600",
+			},
+			yellow: {
+				400: "#ffcc00",
+			},
+			green: {
+				400: "#2acd41",
+			},
+			cyan: {
+				400: "#02c7be",
+			},
+			blue: {
+				300: "#1a85ff",
+				400: "#027aff",
+				500: "#0065d9",
+				600: "#0056b8",
+			},
+			purple: {
+				400: "#b051de",
+			},
+			pink: {
+				400: "#ff2e55",
+			},
+		},
+		extend: {},
+	},
+	plugins: [
+		require("@tailwindcss/typography"),
+		function ({ addVariant }) {
+			addVariant(
+				"prose-inline-code",
+				'&.prose :where(:not(pre)>code):not(:where([class~="not-prose"] *))',
+			);
+		},
+	],
+}
