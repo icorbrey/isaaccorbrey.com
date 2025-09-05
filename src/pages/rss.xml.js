e9fbaedad1fb4e395f8d49e7db8b5a4b6273dca4
Isaac Corbrey <icorbrey@gmail.com>
2025-09-05 15:06:13 -0400

    [wip] notes: Publish RSS feed

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,37 @@
+import { getCollection } from "astro:content";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+import rss from "@astrojs/rss";
+import stripAnsi from "strip-ansi";
+
+const parser = new MarkdownIt();
+
+export async function GET(context) {
+  const notes = await getCollection('notes');
+
+  const renderContent = pipe(
+    parser.render,
+    (v) => sanitizeHtml(v, {
+      allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+    }),
+    stripAnsi,
+  );
+  
+  return rss({
+    title: "Isaac Corbrey",
+    description: "I work on open source projects and occasionally write "
+      + "articles and videos on technical topics.",
+    customData: `<language>en-us</language>`,
+    site: context.site,
+    items: notes.map((post) => ({
+      description: post.data.description,
+      content: renderContent(post.body),
+      pubDate: post.data.publishedOn,
+      link: `/notes/${post.slug}`,
+      title: post.data.title,
+      trailingSlash: false,
+    })),
+  });
+}
+
+const pipe = (...fns) => (x) => fns.reduce((v, f) => f(v), x);
